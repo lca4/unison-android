eb04fa1f2799c914198ac560f13c8440d25849a2
Marc Bourqui <marc.bourqui@epfl.ch>
2013-04-12 09:50:07 +0200

    Clean up after merge.
    Removes menu.xml.orig and strings.xml.orig, fucking up my eclipse.
    Adds a break in UnisonMenu, lost during merge of groupHistory with dev/solo; formats the file.

--- src/ch/epfl/unison/ui/UnisonMenu.java
+++ src/ch/epfl/unison/ui/UnisonMenu.java
@@ -1,3 +1,4 @@
+
 package ch.epfl.unison.ui;
 
 import android.app.Activity;
@@ -14,7 +15,7 @@ import com.actionbarsherlock.view.MenuItem;
 
 /**
  * Abstract base class for all activities that give access to the menu.
- *
+ * 
  * @author lum
  */
 public abstract class UnisonMenu {
@@ -36,42 +37,47 @@ public abstract class UnisonMenu {
     public static boolean onOptionsItemSelected(Activity activity,
             OnRefreshListener listener, MenuItem item) {
         switch (item.getItemId()) {
-        case R.id.menu_item_refresh:
-            if (listener != null) { listener.onRefresh(); }
-        break;
-        case R.id.menu_item_ratings:
-            activity.startActivity(new Intent(activity, RatingsActivity.class));
-        break;
-        case R.id.menu_item_prefs:
-            activity.startActivity(new Intent(activity, PrefsActivity.class));
-        break;
-        case R.id.menu_item_help:
-            activity.startActivity(new Intent(activity, HelpActivity.class));
-        break;
-        
-        case R.id.menu_item_solo:
-            activity.startActivity(new Intent(activity, SoloPlaylistsActivity.class));
-        case R.id.menu_item_history:
+            case R.id.menu_item_refresh:
+                if (listener != null) {
+                    listener.onRefresh();
+                }
+                break;
+            case R.id.menu_item_ratings:
+                activity.startActivity(new Intent(activity, RatingsActivity.class));
+                break;
+            case R.id.menu_item_prefs:
+                activity.startActivity(new Intent(activity, PrefsActivity.class));
+                break;
+            case R.id.menu_item_help:
+                activity.startActivity(new Intent(activity, HelpActivity.class));
+                break;
+
+            case R.id.menu_item_solo:
+                activity.startActivity(new Intent(activity, SoloPlaylistsActivity.class));
+                break;
+            case R.id.menu_item_history:
                 activity.startActivity(new Intent(activity, GroupsHistoryActivity.class).putExtra(
                         Const.Strings.CALLER, activity.getClass().getName()));
                 break;
-        case R.id.menu_item_logout:
-            activity.startActivity(new Intent(activity, LoginActivity.class)
-                    .putExtra(Const.Strings.LOGOUT, true));
-            // Send broadcast to all activities that can only be used when logged in.
-            activity.sendBroadcast(new Intent().setAction(ACTION_LOGOUT));
-        break;
-        case android.R.id.home:
-            // app icon in Action Bar clicked; go home
-            activity.startActivity(new Intent(activity, GroupsActivity.class)
-                    .setAction(GroupsActivity.ACTION_LEAVE_GROUP)
-                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-            // Make sure the activity is finished, even if it was at the bottom of the stack.
-            activity.finish();
-        break;
-        default:
-            // Should never happen.
-            break;
+            case R.id.menu_item_logout:
+                activity.startActivity(new Intent(activity, LoginActivity.class)
+                        .putExtra(Const.Strings.LOGOUT, true));
+                // Send broadcast to all activities that can only be used when
+                // logged in.
+                activity.sendBroadcast(new Intent().setAction(ACTION_LOGOUT));
+                break;
+            case android.R.id.home:
+                // app icon in Action Bar clicked; go home
+                activity.startActivity(new Intent(activity, GroupsActivity.class)
+                        .setAction(GroupsActivity.ACTION_LEAVE_GROUP)
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                // Make sure the activity is finished, even if it was at the
+                // bottom of the stack.
+                activity.finish();
+                break;
+            default:
+                // Should never happen.
+                break;
         }
         return true;
     }
