6f41692dfce4d1e14307a0419ec3fba6a740b39b
Marc Bourqui <marc.bourqui@epfl.ch>
2013-04-15 15:10:58 +0200

    Single-user mode
     - Introduces the Playlist object (empty nutshell for now)

---
+++ src/ch/epfl/unison/Playlist.java
@@ -0,0 +1,12 @@
+package ch.epfl.unison;
+
+/**
+ * Abstraction of a playlist.
+ * A Playlist object could be shared between the single-user mode and groups.
+ * 
+ * @author marc
+ *
+ */
+public class Playlist {
+
+}
