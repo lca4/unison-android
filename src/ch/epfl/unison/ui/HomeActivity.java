f598db73eb8caed63773c2b11add17b449b257d0
Marc Bourqui <marc.bourqui@epfl.ch>
2013-05-30 14:01:43 +0200

    HomeActivity extends SherlockActivity
    Instead of AbstractFragmentActivity, which was not suitable since the Home is
    not intended to host fragments.

--- src/ch/epfl/unison/ui/HomeActivity.java
+++ src/ch/epfl/unison/ui/HomeActivity.java
@@ -4,37 +4,23 @@ package ch.epfl.unison.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
 import ch.epfl.unison.R;
 
-/*
- * TODO
- * 
- * - retrieve playlists stored in android database
- * - store playlist to android database 
- * 
- */
-
 /**
- * Raw copy of GroupsActivity. Has to be properly cleaned up.
  * 
  * @author marc bourqui
  */
-public class HomeActivity extends AbstractFragmentActivity {
+public class HomeActivity extends SherlockActivity {
 
     private static final String TAG = "ch.epfl.unison.HomelistsActivity";
-    private static final int RELOAD_INTERVAL = 120 * 1000; // in ms.
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
-
-        setReloadInterval(RELOAD_INTERVAL);
-    }
-
-    @Override
-    public void onRefresh() {
-        // Do nothing
     }
 
     public void onButtonClickGroups(View view) {
