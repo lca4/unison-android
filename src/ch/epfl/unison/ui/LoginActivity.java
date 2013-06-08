a87fb198f2b6758cf718d50b059d6c27f110c667
Vincent Jalil <vincent.jalil@epfl.ch>
2013-06-08 21:03:53 +0200

    improve app stability. It should be no longer possible to have two instances of the activity running at the same time. This has been tested using NFC only. Note that when starting the loginActivity from another point (like we do in AutoJoin), you should add a clearTop flag so that if the user was already in the loginActivity, it won't be duplicated

--- src/ch/epfl/unison/ui/LoginActivity.java
+++ src/ch/epfl/unison/ui/LoginActivity.java
@@ -55,6 +55,25 @@ public class LoginActivity extends SherlockActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        String action = getIntent().getAction();
+        if (!isTaskRoot() && action != null && action.equals(Intent.ACTION_MAIN)) {
+            
+//          }
+//          if (!isTaskRoot() && ((mExtras != null && !mExtras.getBoolean(Const.Strings.LOGOUT))
+//                  || (mIntentAction != null && !mSupportedActions.contains(mIntentAction)))) {
+              
+              //This test should pass if you are comming from somewhere else in the app
+              //and either you did not press the logout button or you did not try to join a group
+              //when not already logged in (using autojoin) or you did not just created an account.
+              
+              //Here the application has been launched although another instance is already running
+              //but it should not (e.g. the NFC transfer was not done from the right activities).
+              //going to finish this instance to avoid duplicates
+              finish();
+              return;
+          }
+        
         setContentView(R.layout.login);
 
         Log.d(TAG, "Calling LoginActivity.onCreate");
